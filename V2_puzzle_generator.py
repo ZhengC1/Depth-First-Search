ac2199788733a279e5fd5dfea96d295796c1238b
ZhengC1 <zhengchun90@gmail.com>
2016-02-08 01:02:42 -0500

    added a second table to keep track of files

--- V2_puzzle_generator.py
+++ V2_puzzle_generator.py
@@ -13,21 +13,26 @@ import random
 class puzzle_generator(object):
 
     def __init__(self):
+        self.lines = [[0 for x in range(21)] for x in range(21)]
+        self.table = [[1 for x in range(21)] for x in range(21)]
         #Asking the difficulty of the puzzle that they would like to solve
         name_file = raw_input("puzzle difficulty: ")
         map_file = open("Medium.txt", "w+")
         for i in range(21):
-           for j in range(21):
-               if (i == 0 and j == 0) or (i == 20 and j == 20 ):
-                   map_file.write('0')
-               else:
-                   rand_int = random.randint(1,4)
-                   map_file.write(str(rand_int))
-           map_file.write("\n")
-        
+            for j in range(21):
+                #This makes the end position zero a goal
+                if i == 20 and j == 20 :
+                    map_file.write('0')
+                else:
+                    rand_int = random.randint(1,4)
+                    map_file.write(str(rand_int))
+                    self.lines.append(rand_int)
+            map_file.write("\n")
+
         map_file.close()
 
-    def generate_puzzle(self):
-       pass
+    def solve_generate_puzzle(self, x, y, moves):
+        value = int 
+        pass
 cat = puzzle_generator()
 
